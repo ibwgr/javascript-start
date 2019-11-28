d15cc72faa9bc9b4dc69dbc538cc9a5ebebcbac2
Ueli Kunz <kunz@ideadapt.net>
2019-11-28 21:09:30 +0100

    some updates

--- canvas/0-processing.js
+++ canvas/0-processing.js
@@ -7,13 +7,13 @@ function draw() {
     background("beige")
     line(200, 0, 200, 50)
     fill("red")
-    circle(200, 50, 10)
+    circle(200, 55, 10)
 
     line(200, 60, 200, 140)
     fill("green")
-    circle(200, 140, 20)
+    circle(200, 150, 20)
 
     line(200,160, 200, 290)
     fill("blue")
-    circle(200, 280, 30)
+    circle(200, 295, 30)
 }
