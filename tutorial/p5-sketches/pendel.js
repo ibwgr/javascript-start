0164991143f2d49a6f669e73be9a829126d8ba14
Ueli Kunz <kunz@ideadapt.net>
2021-03-18 21:38:03 +0100

    boolean and p5.js start

---
+++ tutorial/p5-sketches/pendel.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400)
+  background("beige")
+  line(200, 0, 200, 50)
+  fill("red")
+  circle(200, 55, 10)
+
+  line(200, 60, 200, 140)
+  fill("green")
+  circle(200, 150, 20)
+
+  line(200,160, 200, 290)
+  fill("blue")
+  circle(200, 295, 30)
+}
