94f619ab30d9869c498868b9a2abde49a5be85b0
Ueli Kunz <kunz@ideadapt.net>
2021-03-19 20:27:11 +0100

    toc to the left, some words about functions, better wording.

--- tutorial/p5-sketches/pendel.js
+++ tutorial/p5-sketches/pendel.js
@@ -1,15 +1,15 @@
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(200, 200)
   background("beige")
-  line(200, 0, 200, 50)
+  line(100, 0, 100, 20)
   fill("red")
-  circle(200, 55, 10)
+  circle(100, 25, 10)
 
-  line(200, 60, 200, 140)
+  line(100, 30, 100, 70)
   fill("green")
-  circle(200, 150, 20)
+  circle(100, 70, 20)
 
-  line(200,160, 200, 290)
+  line(100, 80, 100, 120)
   fill("blue")
-  circle(200, 295, 30)
+  circle(100, 120, 30)
 }
