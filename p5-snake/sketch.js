8da881d211f1f479a61b7df101139e94bddbe54f
Ueli Kunz <kunz@ideadapt.net>
2022-03-17 20:45:44 +0100

    Optimize layout

--- p5-snake/sketch.js
+++ p5-snake/sketch.js
@@ -1,49 +1,103 @@
 // Verwende "Go Live" um diesen sketch im Browser laufen zu lassen.
 
 let bgColorInput
-let rectColorInput
+let snakeColorInput
+let gridSize = 30
+let gameOver = false
+const snake = [
+    {
+        x: 1,
+        y: 0
+    },
+    {
+        x: 2,
+        y: 0
+    }
+]
+const direction = {
+    x: 1,
+    y: 0
+}
 
 function setup(){
-    createCanvas(400, 400)
+    createCanvas(gridSize * 10, gridSize * 10)
+    background('beige')
+    frameRate(1)
 
     drawForm()
 }
 
 function draw(){
     background(bgColorInput.value())
+    fill(snakeColorInput.value())
 
-    fill(rectColorInput.value())
-    rect(50, 50, 100, 100)
+    const letztesX = snake[snake.length -1].x
+    const letztesY = snake[snake.length -1].y
+    
+    if(!gameOver){
+        if(letztesX >= 9 && direction.x === 1){
+            gameOver = true
+            frameRate(0)
+        }else{
+            snake.push({
+                x: letztesX + direction.x,
+                y: letztesY + direction.y,
+            })
+        
+            snake.shift()
+        }
+        for(let bodySegment of snake){
+            rect(bodySegment.x * gridSize, bodySegment.y * gridSize, 
+                gridSize, gridSize)
+        }
+        fill('green')
+        rect(snake[snake.length -1].x * gridSize, snake[snake.length -1].y * gridSize, 
+            gridSize, gridSize)
+        fill(snakeColorInput.value())
+    }
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW && direction.x !== 1) {
+        direction.x = -1
+        direction.y = 0
+    } else if (keyCode === RIGHT_ARROW && direction.x !== -1) {
+        direction.x = 1
+        direction.y = 0
+    } else if (keyCode === DOWN_ARROW  && direction.y !== -1) {
+        direction.x = 0
+        direction.y = 1
+    } else if (keyCode === UP_ARROW && direction.y !== 1) {
+        direction.x = 0
+        direction.y = -1
+    }
 }
 
 function drawForm(){
     /*
     <div>
-        <p>
-            <div>Hintergrundfarbe</div>
-            <bgColorInput />
-        </p>
-        <p>
-            <div>QuadratFarbe</div>
-            <rectColorInput />
-        </p>
-    </div>    
-    */
+        <p>Hintergrundfarbe</p>
+        <bgColorInput />
 
+        <p>QuadratFarbe</p>
+        <rectColorInput />
+    </div>
+    */    
+    
     const div = createDiv()
     div.position(40, 30)
 
+    const pBgColor = createP("Hintergrundfarbe")
+    div.child(pBgColor)
+
     bgColorInput = createColorPicker('beige')
     bgColorInput.size(35)
-    const bgColorP = createP()
-    bgColorP.child(createDiv("Hintergrundfarbe"))
-    bgColorP.child(bgColorInput)
-    div.child(bgColorP)
+    div.child(bgColorInput)
 
-    rectColorInput = createColorPicker('black')
-    rectColorInput.size(35)
-    const snakeColorP = createP()
-    snakeColorP.child(createDiv("Quadratfarbe"))
-    snakeColorP.child(rectColorInput)
-    div.child(snakeColorP)
+    const pSnakeColor = createP("Schlangenfarbe")
+    div.child(pSnakeColor)
+
+    snakeColorInput = createColorPicker('black')
+    snakeColorInput.size(35)
+    div.child(snakeColorInput)
 }
--- tutorial/README.md
+++ tutorial/README.md
@@ -1,7 +1,11 @@
 # Usage
 
-Open js.html in your browser. \
-To enable p5 inline editor serve it via static asset webserver.
+Open index.html in your browser. \
+To enable p5 inline editor serve it via static asset webserver. For example with python:
+
+```python
+python3.8 -m http.server 8000
+```
 
 # Render
 
@@ -11,10 +15,4 @@ To enable p5 inline editor serve it via static asset webserver.
 
 http://eradman.com/entrproject/ must be installed
 
-`./live.sh` \
-`open js.html`
-
-# Todo
-
-* Arrays with illustrations / simple examples
-* Objects with illustrations / simple examples
+`./live.sh`
