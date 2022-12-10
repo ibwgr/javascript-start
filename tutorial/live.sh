b09169e3d85a13208d35c900fc812912940fd09a
Ueli Kunz <kunz@ideadapt.net>
2022-12-10 13:29:03 +0100

    move javascript tutorial to https://github.com/ideadapt/programmieren-lernen-mit-javascript

--- tutorial/live.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name "*.adoc" -or -name "*.css" -or -name "docinfo-*.html" | entr ./render.sh
--- tutorial/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rsync --times --recursive --update --relative --delete --verbose --chmod=F+r \
-./media/*.* \
-./*.css \
-./index.html \
-./p5-sketches/*.{js,css,html} \
-./p5-sketches/libraries/*.js \
-cyon:~/www/programmieren-lernen-mit-javascript.ch
